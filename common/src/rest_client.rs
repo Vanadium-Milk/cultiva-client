ff3eee8db06846ec017b60a54623e35e0fb68597
Leah Castro <leahvcm.01@gmail.com>
2026-03-05 00:48:24 -0600

    Get_evaluation -Added get_evaluation function on rest_client.rs to provide abstraction for the supervision endpoint

--- common/src/rest_client.rs
+++ common/src/rest_client.rs
@@ -1,5 +1,9 @@
+use crate::credentials::get_jwt;
+use crate::db_client::Reading;
+use crate::state_handling::ActivationState;
 use reqwest::{Client, Response};
 use serde::Deserialize;
+use serde_json::json;
 use std::collections::HashMap;
 use std::env::var;
 use std::error::Error;
@@ -23,14 +27,9 @@ pub async fn register_account(
 ) -> Result<Response, Box<dyn Error>> {
     let url = format!("{}/users", var("REST_URL")?);
 
-    let client = Client::new();
-
-    let mut user_register = HashMap::new();
-    user_register.insert("email", email);
-    user_register.insert("password", password);
-    user_register.insert("name", username);
-
-    let res = client.post(url).json(&user_register).send().await?;
+    let user_register =
+        HashMap::from([("email", email), ("password", password), ("name", username)]);
+    let res = Client::new().post(url).json(&user_register).send().await?;
 
     Ok(res)
 }
@@ -38,12 +37,33 @@ pub async fn register_account(
 pub async fn login_account(email: &str, password: &str) -> Result<Response, Box<dyn Error>> {
     let url = format!("{}/users/login", var("REST_URL")?);
 
-    let client = Client::new();
-    let mut user_login = HashMap::new();
-    user_login.insert("email", email);
-    user_login.insert("password", password);
-
-    let res = client.post(url).json(&user_login).send().await?;
+    let user_login = HashMap::from([("email", email), ("password", password)]);
+    let res = Client::new().post(url).json(&user_login).send().await?;
 
     Ok(res)
+}
+
+pub async fn get_evaluation(
+    readings: Vec<Reading>,
+    context: HashMap<String, String>,
+    activation: ActivationState,
+    image: String,
+) -> Result<Response, Box<dyn Error>> {
+    let url = format!("{}/supervision", var("REST_URL")?);
+
+    //json! macro includes None values as null, I converted it to HashMap first to remove them
+    let clean_act: HashMap<String, bool> = activation.into();
+    let content = json!({
+        "readings": readings,
+        "context": context,
+        "activation": clean_act,
+        "image": image
+    });
+    let response = Client::new()
+        .post(url)
+        .json(&content)
+        .bearer_auth(get_jwt()?)
+        .send()
+        .await?;
+    Ok(response)
 }
